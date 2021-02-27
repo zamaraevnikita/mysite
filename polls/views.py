b98e541e527ca6d3de26bbe269df2a90fceb34fb
zamaraevnikita <nikzam241104@gmail.com>
2021-02-27 17:35:12 +0700

    add meme_r2

--- polls/views.py
+++ polls/views.py
@@ -7,4 +7,4 @@ def index(request):
 
 
 def meme(request):
-    return HttpResponse('img src="https://n1s1.hsmedia.ru/c8/9f/cb/c89fcb31dd077084bc8bbc2284586b7f/1000x600_0xac120003_16946826431608901545.jpg"')
+    return HttpResponse('<img src="https://n1s1.hsmedia.ru/c8/9f/cb/c89fcb31dd077084bc8bbc2284586b7f/1000x600_0xac120003_16946826431608901545.jpg">')
