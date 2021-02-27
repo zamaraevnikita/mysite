fc5dcd93ac19a4c342aa7d6cb2d332421b5c33b8
zamaraevnikita <nikzam241104@gmail.com>
2021-02-27 17:31:22 +0700

    add meme

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ne0(lop26p@py_xk)!y4d&32p+3l!f+jtick%unb_(7c3k!_pj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["Orie.pythonanywhere.com"]
+ALLOWED_HOSTS = ["Orie.pythonanywhere.com", '127.0.0.1' , 'localhost']
 
 
 # Application definition
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1> hallo </h1>")
+
+
+def meme(request):
+    return HttpResponse('img src="https://n1s1.hsmedia.ru/c8/9f/cb/c89fcb31dd077084bc8bbc2284586b7f/1000x600_0xac120003_16946826431608901545.jpg"')
